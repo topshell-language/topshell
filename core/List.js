175e022b2f8013cb15c5681da5ed70ed9e20ee3f
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2018-08-11 10:06:15 +0200

    More functions in List.js

--- core/List.js
+++ core/List.js
@@ -23,11 +23,22 @@ exports.then_ = function(f) {
 };
 
 exports.size_ = function(r) { return r.length; };
+exports.isEmpty_ = function(r) { return r.length === 0; };
 exports.at_ = function(r) { return function(i) { return r[i]; }; };
 exports.take_ = function(r) { return function(i) { return r.slice(0, i); }; };
 exports.drop_ = function(r) { return function(i) { return r.slice(i); }; };
 exports.takeLast_ = function(r) { return function(i) { return r.slice(-i); }; };
 exports.dropLast_ = function(r) { return function(i) { return r.slice(0, -i); }; };
+
+exports.filter_ = function(f) { return function(r) { return r.filter(f); }; };
+exports.reverse_ = function(r) { return r.slice().reverse(); };
+exports.find_ = function(f) { return function(r) { return r.find(f); }; };
+exports.all_ = function(f) { return function(r) { return r.every(f); }; };
+exports.any_ = function(f) { return function(r) { return r.some(f); }; };
+
+exports.head_ = function(r) { return r[0]; };
+exports.tail_ = function(r) { return r.slice(1); };
+exports.append_ = function(r) { return function(a) { return r.concat(a); }; };
 
 exports.takeWhile_ = function(r) { return r.takeWhile_; };
 exports.dropWhile_ = function(r) { return r.dropWhile_; };
@@ -36,20 +47,11 @@ exports.dropLastWhile_ = function(r) { return r.dropLastWhile_; };
 exports.zip_ = function(r) { return r.zip_; };
 exports.unzip_ = function(r) { return r.unzip_; };
 exports.indexes_ = function(r) { return r.indexes_; };
-exports.filter_ = function(r) { return r.filter_; };
 exports.startsWith_ = function(r) { return r.startsWith_; };
 exports.endsWith_ = function(r) { return r.endsWith_; };
-exports.find_ = function(r) { return r.find_; };
-exports.any_ = function(r) { return r.any_; };
-exports.all_ = function(r) { return r.all_; };
-exports.empty_ = function(r) { return r.empty_; };
-exports.reverse_ = function(r) { return r.reverse_; };
 exports.join_ = function(r) { return r.join_; };
 exports.sort_ = function(r) { return r.sort_; };
 
-exports.head_ = function(r) { return r[0]; };
-exports.tail_ = function(r) { return r.slice(1); };
-exports.append_ = function(r) { return function(a) { return r.concat(a); }; };
 
 exports.foldLeft_ = function(r) { return r.foldLeft_; };
 exports.foldRight_ = function(r) { return r.foldRight_; };
