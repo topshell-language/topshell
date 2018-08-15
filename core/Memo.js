bf2511603801dd8c92448f6a29046675b41dca7f
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2018-08-15 08:37:40 +0200

    Memo

---
+++ core/Memo.js
@@ -0,0 +1,13 @@
+exports.dictionaryBy_ = g => f => v => {
+    var cache = {};
+    function h(x) {
+        var k = g(x);
+        if(!Object.prototype.hasOwnProperty.call(cache, k)) cache[k] = f(h)(x);
+        console.dir(cache);
+        return cache[k];
+    }
+    return h(v);
+};
+
+exports.dictionary_ = exports.dictionaryBy_(k => "" + k);
+
