9655713362f7730fdb653abec4087620c8e80c63
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2018-10-09 23:26:21 +0200

    Type system start

--- core/Maybe.js
+++ core/Maybe.js
@@ -1,14 +1,34 @@
+//: Maybe a
 exports.none = self.tsh.none;
+
+//: a -> Maybe a
 exports.some = self.tsh.some;
+
+//: Maybe a -> Bool
 exports.isNone = self.tsh.isNone;
+
+//: Maybe a -> Bool
 exports.isSome = self.tsh.isSome;
+
+//: a -> Maybe a -> a
 exports.default = d => v => exports.isNone(v) ? d : v.value;
+
+//: Maybe (Maybe a) -> Maybe a
 exports.flatten = v => exports.isNone(v) ? v : v.value;
+
+//: (a -> b) -> Maybe a -> Maybe b
 exports.map = f => v => exports.isNone(v) ? v : exports.some(f(v.value));
+
+//: (a -> Maybe b) -> Maybe a -> Maybe b
 exports.flatMap = f => v => exports.isNone(v) ? v : f(v.value);
+
+//: Maybe a -> List a
 exports.toList = v => exports.isNone(v) ? [] : [v.value];
+
+//: String -> Maybe a -> a
 exports.orFail = e => v => {if(exports.isNone(v)) throw e; else return v;};
 
+//: List (Maybe a) -> List a
 exports.flattenList = v => {
     var result = [];
     for(var i = 0; i < v.length; i++) {
